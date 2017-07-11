d3eff218e20ba5fda7635cb3f26f4a351a4615dc
Alexander Gabriel <alex@gabriel-software.ch>
2017-07-11 22:22:08 +0200

    functionalize

--- src/components/Events/IntroJumbotron.js
+++ src/components/Events/IntroJumbotron.js
@@ -2,7 +2,7 @@
  * can't convert to es6 function because this is referenced
  */
 
-import React, { Component } from 'react'
+import React from 'react'
 import { Jumbotron } from 'react-bootstrap'
 import styled from 'styled-components'
 
@@ -24,21 +24,15 @@ const P = styled.p`
   font-size: 18px !important;
 `
 
-class IntroJumbotron extends Component {
-  displayName: 'IntroJumbotron'
+const IntroJumbotron = () =>
+  <StyledJumbotron className="eventsIntro">
+    <P>
+      Brexit is a topic of historical proportions and difficult to get an
+      overview. This website is meant to help. It is a simple chronology
+      beginning with the British exit notice the end of March 2017. The focus is
+      on the negotiation dynamics between the two sides based on information of
+      a handful of publicly available sources.
+    </P>
+  </StyledJumbotron>
 
-  render() {
-    return (
-      <StyledJumbotron className="eventsIntro">
-        <P>
-          Brexit is a topic of historical proportions and difficult to get an
-          overview. This website is meant to help. It is a simple chronology
-          beginning with the British exit notice the end of March 2017. The
-          focus is on the negotiation dynamics between the two sides based on
-          information of a handful of publicly available sources.
-        </P>
-      </StyledJumbotron>
-    )
-  }
-}
 export default IntroJumbotron
