db22d34a89ffbd2f8d5392219fd434b924ac1da9
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-05 13:08:46 +0100

    use hooks

--- src/components/Events/EditEventGlyph.js
+++ src/components/Events/EditEventGlyph.js
@@ -1,10 +1,10 @@
 // @flow
-import React from 'react'
+import React, { useCallback, useContext } from 'react'
 import { Glyphicon, Tooltip, OverlayTrigger } from 'react-bootstrap'
-import { observer, inject } from 'mobx-react'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
+import { observer } from 'mobx-react'
 import styled from 'styled-components'
+
+import storeContext from './../../storeContext'
 
 const StyledGlyphicon = styled(Glyphicon)`
   font-size: 0.9em;
@@ -12,32 +12,25 @@ const StyledGlyphicon = styled(Glyphicon)`
   cursor: pointer;
 `
 
-const enhance = compose(
-  inject(`store`),
-  withHandlers({
-    onClick: props => () => {
-      props.store.events.getEvent(props.event._id)
+const EditEventGlyph = ({ event }: { event: Object }) => {
+  const store = useContext(storeContext)
+  const onClick = useCallback(
+    () => {
+      store.events.getEvent(event._id)
     },
-  }),
-  observer
-)
+    [event._id],
+  )
 
-const EditEventGlyph = ({
-  store,
-  event,
-  onClick,
-}: {
-  store: Object,
-  event: Object,
-  onClick: () => void,
-}) =>
-  <OverlayTrigger
-    placement="top"
-    overlay={<Tooltip id="editThisEvent">edit</Tooltip>}
-  >
-    <StyledGlyphicon glyph="pencil" onClick={onClick} />
-  </OverlayTrigger>
+  return (
+    <OverlayTrigger
+      placement="top"
+      overlay={<Tooltip id="editThisEvent">edit</Tooltip>}
+    >
+      <StyledGlyphicon glyph="pencil" onClick={onClick} />
+    </OverlayTrigger>
+  )
+}
 
 EditEventGlyph.displayName = 'EditEventGlyph'
 
-export default enhance(EditEventGlyph)
+export default observer(EditEventGlyph)
