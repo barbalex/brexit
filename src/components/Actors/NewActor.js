23f2112d5c19538485e11527077cf7b14ff6a7e0
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-05 09:48:35 +0100

    use hooks

--- src/components/Actors/NewActor.js
+++ src/components/Actors/NewActor.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react'
+import React, { useState, useCallback, useContext } from 'react'
 import {
   Modal,
   Button,
@@ -8,83 +8,64 @@ import {
   ControlLabel,
   FormControl,
 } from 'react-bootstrap'
-import { observer, inject } from 'mobx-react'
-import compose from 'recompose/compose'
-import withState from 'recompose/withState'
-import withHandlers from 'recompose/withHandlers'
+import { observer } from 'mobx-react'
 import styled from 'styled-components'
+
+import storeContext from '../../storeContext'
 
 const ErrorAlert = styled(Alert)`
   magrin-bottom: 10px;
 `
 
-const enhance = compose(
-  inject(`store`),
-  withState('category', 'changeCategory', ''),
-  withState('error', 'changeError', null),
-  withHandlers({
-    onChangeCategory: props => (event: Object): void =>
-      props.changeCategory(event.target.value),
-    createNewActor: props => () => {
-      const { category } = props
-      if (category) {
-        props.store.actors.newActor(category)
-        props.store.actors.setShowNewActor(false)
-      } else {
-        props.changeError('Please choose a category')
-      }
-    },
-    closeNewActor: props => () => {
-      props.store.actors.setShowNewActor(false)
-    },
-  }),
-  observer
-)
+const NewActor = () => {
+  const store = useContext(storeContext)
+  const { newActor, setShowNewActor } = store.actors
+  const [category, setCategory] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const onChangeCategory = useCallback(
+    (event: Object): void => setCategory(event.target.value),
+  )
+  const createNewActor = useCallback(() => {
+    if (category) {
+      newActor(category)
+      setShowNewActor(false)
+    } else {
+      setErrorMessage('Please choose a category')
+    }
+  })
+  const closeNewActor = useCallback(() => setShowNewActor(false))
 
-const NewActor = ({
-  store,
-  category,
-  error,
-  closeNewActor,
-  onChangeCategory,
-  createNewActor,
-}: {
-  store: Object,
-  category: string,
-  error: string,
-  closeNewActor: () => void,
-  onChangeCategory: () => void,
-  createNewActor: () => void,
-}) =>
-  <Modal show bsSize="large">
-    <Modal.Header>
-      <Modal.Title>New actor category</Modal.Title>
-    </Modal.Header>
+  return (
+    <Modal show bsSize="large">
+      <Modal.Header>
+        <Modal.Title>New actor category</Modal.Title>
+      </Modal.Header>
 
-    <Modal.Body>
-      <FormGroup controlId="actorCategory">
-        <ControlLabel>Category</ControlLabel>
-        <FormControl
-          type="text"
-          value={category}
-          onChange={onChangeCategory}
-          autoFocus
-        />
-      </FormGroup>
-      {error &&
-        <ErrorAlert bsStyle="danger">
-          {error}
-        </ErrorAlert>}
-    </Modal.Body>
+      <Modal.Body>
+        <FormGroup controlId="actorCategory">
+          <ControlLabel>Category</ControlLabel>
+          <FormControl
+            type="text"
+            value={category}
+            onChange={onChangeCategory}
+            autoFocus
+          />
+        </FormGroup>
+        {errorMessage && (
+          <ErrorAlert bsStyle="danger">{errorMessage}</ErrorAlert>
+        )}
+      </Modal.Body>
 
-    <Modal.Footer>
-      <Button onClick={closeNewActor}>discard input and close</Button>
-      <Button bsStyle="primary" onClick={createNewActor}>
-        create new actor
-      </Button>
-    </Modal.Footer>
-  </Modal>
+      <Modal.Footer>
+        <Button onClick={closeNewActor}>discard input and close</Button>
+        <Button bsStyle="primary" onClick={createNewActor}>
+          create new actor
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
 
 NewActor.displayName = 'NewActor'
 
-export default enhance(NewActor)
+export default observer(NewActor)
