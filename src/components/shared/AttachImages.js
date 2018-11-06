421de5e1f80503f0f17c511f4faa88e7cd3910ad
Alexander Gabriel <alex.barbalex@gmail.com>
2018-11-06 13:49:14 +0100

    use hooks

--- src/components/shared/AttachImages.js
+++ src/components/shared/AttachImages.js
@@ -1,10 +1,10 @@
 // @flow
-import React from 'react'
+import React, { useCallback, useContext } from 'react'
 import Dropzone from 'react-dropzone'
-import { observer, inject } from 'mobx-react'
-import compose from 'recompose/compose'
-import withHandlers from 'recompose/withHandlers'
+import { observer } from 'mobx-react'
 import styled from 'styled-components'
+
+import storeContext from '../../storeContext'
 
 const Container = styled.div`
   padding-top: 10px;
@@ -21,31 +21,30 @@ const DropzoneDiv = styled.div`
   padding: 5px;
 `
 
-const enhance = compose(
-  inject(`store`),
-  withHandlers({
-    onDrop: props => files => {
-      const { store, doc } = props
+const AttachImages = ({ doc }: { doc: Object }) => {
+  const store = useContext(storeContext)
+  const onDrop = useCallback(
+    files => {
       const attachments = {}
       files.forEach(file => {
         /**
-       * create an attachments object of this form:
-       * {
-       *   "att.txt": {
-       *     "content_type": "image/png",
-       *     "data": new Blob(
-       *       ["And she's hooked to the silver screen"],
-       *       {type: 'text/plain'})
-       *   },
-       *   "att2.txt": {
-       *     "content_type": "text/plain",
-       *     "data": new Blob(
-       *       ["But the film is a saddening bore"],
-       *       {type: 'text/plain'})
-       *   }
-       * }
-       * note: react-dropzone built the blob itself! It is file
-       */
+         * create an attachments object of this form:
+         * {
+         *   "att.txt": {
+         *     "content_type": "image/png",
+         *     "data": new Blob(
+         *       ["And she's hooked to the silver screen"],
+         *       {type: 'text/plain'})
+         *   },
+         *   "att2.txt": {
+         *     "content_type": "text/plain",
+         *     "data": new Blob(
+         *       ["But the film is a saddening bore"],
+         *       {type: 'text/plain'})
+         *   }
+         * }
+         * note: react-dropzone built the blob itself! It is file
+         */
         attachments[file.name] = {
           content_type: file.type,
           data: file,
@@ -54,28 +53,22 @@ const enhance = compose(
 
       store.page.addPageAttachments(doc, attachments)
     },
-  }),
-  observer
-)
+    [store.page],
+  )
 
-const AttachImages = ({
-  store,
-  doc,
-  onDrop,
-}: {
-  store: Object,
-  doc: Object,
-  onDrop: () => void,
-}) =>
-  <Container>
-    <Dropzone onDrop={onDrop} accept="image/*">
-      <DropzoneDiv>
-        Drop some images here.<br />
-        Or click to select.
-      </DropzoneDiv>
-    </Dropzone>
-  </Container>
+  return (
+    <Container>
+      <Dropzone onDrop={onDrop} accept="image/*">
+        <DropzoneDiv>
+          Drop some images here.
+          <br />
+          Or click to select.
+        </DropzoneDiv>
+      </Dropzone>
+    </Container>
+  )
+}
 
 AttachImages.displayName = 'AttachImages'
 
-export default enhance(AttachImages)
+export default observer(AttachImages)
